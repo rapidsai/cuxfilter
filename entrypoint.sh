ad3f1014e442b90c92a47bd5557bb5491f915e23
athorve <athorve@nvidia.com>
2018-08-29 15:58:00 -0700

    file structure changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,11 @@
 source activate gdf
 export FLASK_ENV=development
-export FLASK_APP=../python_scripts/flask_server/run.py
+export FLASK_APP=../flask_server/run.py
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-flask run --port=3002 &
-cd ../nvidia-smi-gadget
-npm start &
-cd ../node_server
+## uncomment the below 4 lines if you want to run the nvidia-smi gadget for the front-end
+# flask run --port=3002 &
+# cd ../nvidia-smi-gadget
+# npm start &
+# cd ../node_server
 npm start
-# ls
-# cd ../
-# jupyter notebook --ip 0.0.0.0 --port 3004 --allow-root &
