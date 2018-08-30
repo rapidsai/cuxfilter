e1e5c3556ef5516c52133a7a71fe4e30ad1d1856
athorve <athorve@nvidia.com>
2018-08-30 14:26:10 -0700

    updated entrypoint for Docker container

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,14 @@ export FLASK_ENV=development
 export FLASK_APP=../flask_server/run.py
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
+flask run --port=3002 &
+
 ## uncomment the below 4 lines if you want to run the nvidia-smi gadget for the front-end
-# flask run --port=3002 &
 # cd ../nvidia-smi-gadget
 # npm start &
 # cd ../node_server
 npm start
+
+
+# cd ../
+# jupyter notebook --ip 0.0.0.0 --port 3004 --allow-root &
