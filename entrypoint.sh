e2198d77e3d837d8e437e206766606b7ef703ef5
athorve <athorve@nvidia.com>
2018-08-31 15:56:53 -0700

    separate servers for pandas and pygdf runtime engines

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ export FLASK_APP=../flask_server/run.py
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 flask run --port=3002 &
-
+flask run --port=3003 &
 # uncomment the below 4 lines if you want to run the nvidia-smi gadget for the front-end
 cd ../nvidia-smi-gadget
 npm start &
