95832757208e37234097c63fdccd79ba97783134
athorve <athorve@nvidia.com>
2018-10-02 11:22:22 -0700

    added comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,14 @@ export FLASK_ENV=development
 export FLASK_APP=../flask_server/run.py
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
+
+#pygdf engine
 flask run --port=3002 &
+
+#pandas engine
 flask run --port=3003 &
+
+
 # uncomment the below 4 lines if you want to run the nvidia-smi gadget for the front-end
 cd ../nvidia-smi-gadget
 npm start &
