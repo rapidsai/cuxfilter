57866380adbe330b19394608b98f85fd2afbe60e
athorve <athorve@nvidia.com>
2018-10-16 17:28:14 -0700

    added process manager(pm2) to handle server components -node and flask

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,23 @@
 source activate gdf
+
 export FLASK_ENV=development
-export FLASK_APP=../flask_server/run.py
+export FLASK_APP=./flask_server/run.py
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
+pygdf_port=`cat config.json | jq --raw-output '.flask_server_port_pygdf'`
+pandas_port=`cat config.json | jq --raw-output '.flask_server_port_pandas'`
+
 #pygdf engine
-flask run --port=3002 &
+pm2 start "flask run --port=$pygdf_port"
 
 #pandas engine
-flask run --port=3003 &
+pm2 start "flask run --port=$pandas_port"
 
-cd ../node_server
-npm start
+cd ./node_server
+pm2 start npm -- start
 
-
+pm2 logs
 #uncomment below two lines to run a jupyter notebook intance
 #cd ../
 #jupyter notebook --ip 0.0.0.0 --port 3004 --allow-root &
