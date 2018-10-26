dec0d365245d959491c595086d27e511785f2079
Ajay Thorve <athorve@nvidia.com>
2018-10-26 23:01:24 +0100

    remove unused files

--- flask_server/app/utilities/tests/test_pygdfCrossfilter_utils.py
+++
@@ -1,7 +0,0 @@
-import pygdfCrossfilter_utils as pygdf
-
-class pygdfCrossfilter_utils_test():
-    def test_read(self):
-        result = pygdf.read_data('arrow','../../../../node_server/uploads/predictions-v1')
-        assert(result,'data read successfully')
-        
