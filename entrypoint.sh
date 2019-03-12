2912633ade96d3abb82d0c6b0a83ebc4b8fa9ada
athorve <athorve@nvidia.com>
2019-03-12 13:33:15 -0700

    updated to remove port dependency and added service nginx start

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,38 +1,27 @@
+#activating the conda environment
 source activate cudf
-
-# export sanic_ENV=production
-# export sanic_APP=./sanic_server/run.py
-# export LC_ALL=C.UTF-8
-# export LANG=C.UTF-8
-
-cudf_port=`cat config.json | jq --raw-output '.sanic_server_port_cudf_internal'`
-pandas_port=`cat config.json | jq --raw-output '.sanic_server_port_pandas_internal'`
-jupyter_port=`cat config.json | jq --raw-output '.jupyter_port'`
-demos_serve_port_internal=3004
 
 cd ./sanic_server
 #cudf engine
-# pm2 start "sanic run --port=$cudf_port" --watch
-pm2 start "python -m sanic app.app --port=$cudf_port" --watch
+pm2 start "python -m sanic app.app --port=$sanic_server_port_cudf" --watch
 
 #pandas engine
-# pm2 start "sanic run --port=$pandas_port" --watch
-pm2 start "python -m sanic app.app --port=$pandas_port" --watch
+pm2 start "python -m sanic app.app --port=$sanic_server_port_pandas" --watch
 
+#cuxfilter engine
 cd ../node_server
 pm2 start npm -- start --watch
 
+#gtc demo
 cd '../demos/GTC demo/'
-npm run start &
+pm2 start "npm start --port=$gtc_demo_port" --watch
 
+#jupyter notebook server
 cd ../Jupyter_Integration
-jupyter notebook --ip 0.0.0.0 --port $jupyter_port --allow-root --NotebookApp.token='' &
+jupyter notebook --ip 0.0.0.0 --port $jupyter_port --allow-root --NotebookApp.token='' --NotebookApp.base_url='/jupyter' &
 
+#starting nginx reverse-proxying
+service nginx start
 
-cd ../../
-pm2 serve --port=$demos_serve_port_internal
-
+#genrating all server logs
 pm2 logs
-#uncomment below two lines to run a jupyter notebook intance
-#cd ../
-#jupyter notebook --ip 0.0.0.0 --port 3004 --allow-root &
