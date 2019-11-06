c75b879a8022854b61d464106b4ae619ea839fcd
Ajay Thorve <athorve@nvidia.com>
2019-11-06 11:01:42 -0800

    added more tests for dashboard.py

--- python/cuXfilter/tests/test_dashboard.py
+++ python/cuXfilter/tests/test_dashboard.py
@@ -3,6 +3,8 @@ import pytest
 from cuXfilter.dashboard import DashBoard
 import cuXfilter
 import cudf
+import pandas as pd
+import numpy as np
 
 class TestDataFrame():
 
@@ -13,7 +15,7 @@ class TestDataFrame():
     dashboard = cux_df.dashboard(charts=[], title='test_title')
 
     def test_variables(self):
-        assert self.dashboard._data.to_string() == self.df.to_string()
+        assert self.dashboard._data.equals(self.df)
         assert self.dashboard.title == 'test_title'
         assert self.dashboard._dashboard.__class__ == cuXfilter.layouts.single_feature
         assert self.dashboard._theme == cuXfilter.themes.light
@@ -55,7 +57,7 @@ class TestDataFrame():
         if result2 is not None:
             assert dashboard._data.to_string() == result2
         else:
-            assert dashboard._data.to_string() == df.to_string()
+            assert dashboard._data.equals(df)
 
 
     @pytest.mark.parametrize('query_dict, query_str',[
@@ -83,14 +85,98 @@ class TestDataFrame():
     #unit tests for datatile and query functions are already present in core_aggregate and core_non_aggregate test files
 
     #integrated tests
-
-    def test_calc_data_tiles(self):
+    @pytest.mark.parametrize('active_view, passive_view, result',[
+        ('key_line', 'val_bar', pd.DataFrame(
+                {0: {0: 1.0, 1: 0.0, 2: 0.0, 3: 0.0, 4: 0.0},
+                1: {0: 1.0, 1: 1.0, 2: 0.0, 3: 0.0, 4: 0.0},
+                2: {0: 1.0, 1: 1.0, 2: 1.0, 3: 0.0, 4: 0.0},
+                3: {0: 1.0, 1: 1.0, 2: 1.0, 3: 1.0, 4: 0.0},
+                4: {0: 1.0, 1: 1.0, 2: 1.0, 3: 1.0, 4: 1.0}}
+            )
+        ),
+        ('val_bar', 'key_line', None)
+    ])
+    def test_calc_data_tiles(self, active_view, passive_view, result):
         df = cudf.DataFrame(
-            {'key': [0, 1, 2, 2, 2], 'val':[float(i + 10) for i in range(5)]}
+            {'key': [0, 1, 2, 3, 4], 'val':[float(i + 10) for i in range(5)]}
             )
         cux_df = cuXfilter.DataFrame.from_dataframe(df)
-        bac = cuXfilter.charts.bokeh.bar('key')
+        bac = cuXfilter.charts.bokeh.line('key', 'val')
+        bac.use_data_tiles = False
         bac1 = cuXfilter.charts.bokeh.bar('val')
         dashboard = cux_df.dashboard(charts=[bac, bac1], title='test_title', layout=cuXfilter.layouts.double_feature)
+        dashboard._active_view = active_view
+        dashboard._calc_data_tiles()
+
+        if result is None:
+            assert dashboard._data_tiles[passive_view] is result
+        else:
+            assert dashboard._data_tiles[passive_view].equals(result)
+
+        assert dashboard._charts[dashboard._active_view].datatile_loaded_state == True
+
+    @pytest.mark.parametrize( 'query_tuple, result', [
+        ((2,4),  [0, 0, 1, 1]),
+        ((2,2), [0, 0, 1, 0]),
+        ((0,0), [1, 0, 0, 0]),
+        ((0,4), [1, 1, 1, 1])
+    ])
+    def test_query_datatiles_by_range(self, query_tuple, result):
+        df = cudf.DataFrame(
+            {'key': [0, 1, 2, 3, 4], 'val':[float(i + 10) for i in range(5)]}
+            )
+        cux_df = cuXfilter.DataFrame.from_dataframe(df)
+        bac = cuXfilter.charts.bokeh.line('key', 'val')
+        bac1 = cuXfilter.charts.bokeh.bar('val')
+        dashboard = cux_df.dashboard(charts=[bac, bac1], title='test_title', layout=cuXfilter.layouts.double_feature)
+        dashboard._active_view = bac.name
+        dashboard._calc_data_tiles()
+        dashboard._query_datatiles_by_range(query_tuple=query_tuple)
+
+        assert all(bac1.source.data['top'] == result)
 
 
+    @pytest.mark.parametrize('old_indices, new_indices, prev_result, result', [
+        ([], [1], [1, 1, 1, 2], [0, 1, 0, 0]),
+        ([1], [1], [0, 1, 0, 0], [0, 1, 0, 0]),
+        ([1], [1, 2], [0, 1, 0, 0], [0, 1, 1, 0]),
+        ([1, 2], [2], [0, 1, 1, 0], [0, 0, 1, 0])
+    ])
+    def test_query_datatiles_by_indices(self, old_indices, new_indices, prev_result, result):
+        df = cudf.DataFrame(
+            {'key': [0, 1, 2, 3, 4], 'val':[float(i + 10) for i in range(5)]}
+            )
+        cux_df = cuXfilter.DataFrame.from_dataframe(df)
+        bac = cuXfilter.charts.bokeh.line('key', 'val')
+        bac1 = cuXfilter.charts.bokeh.bar('val')
+        dashboard = cux_df.dashboard(charts=[bac, bac1], title='test_title', layout=cuXfilter.layouts.double_feature)
+        dashboard._active_view = bac.name
+        dashboard._calc_data_tiles(cumsum=False)
+
+        bac1.source.data['top'] = np.array(prev_result)
+        dashboard._query_datatiles_by_indices(old_indices=old_indices, new_indices=new_indices)
+        
+        assert all(bac1.source.data['top'] == result)
+
+    
+    def test_reset_current_view(self):
+        df = cudf.DataFrame(
+            {'key': [0, 1, 2, 3, 4], 'val':[float(i + 10) for i in range(5)]}
+            )
+        cux_df = cuXfilter.DataFrame.from_dataframe(df)
+        bac = cuXfilter.charts.bokeh.line('key', 'val')
+        bac1 = cuXfilter.charts.bokeh.bar('val')
+        dashboard = cux_df.dashboard(charts=[bac, bac1], title='test_title', layout=cuXfilter.layouts.double_feature)
+        dashboard._active_view = bac.name
+        dashboard._calc_data_tiles()
+        dashboard._query_datatiles_by_range(query_tuple=(1,2))
+        bac.filter_widget.value = (1,2)
+
+        #reset active view
+        dashboard._reset_current_view(new_active_view=bac1)
+
+        assert dashboard._active_view == bac1.name
+        assert dashboard._query_str_dict == {'key_line': '1<=key<=2'}
+        assert dashboard._charts[bac.name].datatile_loaded_state == False
+        assert bac1.name not in dashboard._query_str_dict
+        assert dashboard._data.equals(df.query(dashboard._query_str_dict['key_line']))
