c8d78d69dbd5405556ac202743c92dbefcd84ec3
Ajay Thorve <athorve@nvidia.com>
2019-11-13 16:57:26 -0800

    bug fixes in core charts

--- python/cuXfilter/charts/core/core_chart.py
+++ python/cuXfilter/charts/core/core_chart.py
@@ -86,7 +86,6 @@ class BaseChart:
         self.set_color()
 
     def set_color(self):
-        self.color = None
         if 'color' in self.library_specific_params:
             self.color = self.library_specific_params['color']
 
