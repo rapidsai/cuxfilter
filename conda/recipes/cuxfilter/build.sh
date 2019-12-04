20b52dfeb189689cadc3d22e939ac91e2afedc12
Dillon Cullinan <dcullinan@dgx01.aselab.nvidia.com>
2019-12-04 12:18:17 -0800

    Test with env variable

--- conda/recipes/cuxfilter/build.sh
+++ conda/recipes/cuxfilter/build.sh
@@ -2,4 +2,4 @@
 
 # This assumes the script is executed from the root of the repo directory
 pip install git+https://github.com/rapidsai/cuDataShader.git --upgrade --no-deps
-./build.sh cuxfilter
+${WORKSPACE}/build.sh cuxfilter
