5e0aa4925b883a6d1b6d12e35e4e01ecd403d638
Ajay Thorve <athorve@nvidia.com>
2020-01-28 15:40:36 -0800

    using IPython display instead of ipywidgets

--- python/cuxfilter/assets/notebook_assets.py
+++ python/cuxfilter/assets/notebook_assets.py
@@ -1,13 +1,9 @@
 import panel as pn
-import ipywidgets as widgets
-
-out = widgets.Output(layout={"border": "1px solid black"})
+from IPython import display
 
 
 def load_notebook_assets():
-    with out:
-        print("loading cuxfilter notebook assets ... ")
+    print("loading cuxfilter notebook assets ... ")
     pn.extension()
-    out.clear_output()
-    with out:
-        print("cuxfilter notebook assets successfully loaded.")
+    display.clear_output()
+    print("cuxfilter notebook assets successfully loaded.")
