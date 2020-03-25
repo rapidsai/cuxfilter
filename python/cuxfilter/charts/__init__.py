9760fa32da889c490417b2181ba8d64513e9c737
Ajay Thorve <athorve@nvidia.com>
2020-03-24 23:32:06 -0700

    refactoring chart imports - removing library names

--- python/cuxfilter/charts/__init__.py
+++ python/cuxfilter/charts/__init__.py
@@ -1,5 +1,17 @@
-from .bokeh import bokeh
-from .datashader import datashader
-from .deckgl import deckgl
-from .panel_widgets import panel_widgets
+# from .bokeh import bokeh
+# from .datashader import datashader
+# from .deckgl import deckgl
+# from .panel_widgets import panel_widgets
+
+from .bokeh import bar, line
+from .datashader import scatter, scatter_geo, stacked_lines, heatmap
+from .deckgl import choropleth
+from .panel_widgets import (
+    range_slider,
+    int_slider,
+    float_slider,
+    multi_select,
+    drop_down,
+    data_size_indicator,
+)
 from .core.core_view_dataframe import ViewDataFrame as view_dataframe
