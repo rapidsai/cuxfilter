727fe50799f2a354223fb1b68d834cb5b70911e5
Raymond Douglass <ray@raydouglass.com>
2020-03-27 12:14:53 -0400

    FIX Let rapids-env dictate versions

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -58,9 +58,8 @@ nvidia-smi
 logger "Activate conda env..."
 source activate gdf
 conda install "cudf=$MINOR_VERSION.*" "cudatoolkit=$CUDA_REL" "rapids-notebook-env=$MINOR_VERSION.*" \
-              "numpy>=1.16" "cupy>=6.0.0" "datashader>=0.10.*" "panel=0.6.*" \
-              "bokeh=1.*" "geopandas>=0.6.*" "pytest" "pyppeteer" "jupyter-server-proxy"\
-              "pyproj>=2.4.*"
+              "datashader>=0.10.*" "panel=0.6.*" "bokeh=1.*" "geopandas>=0.6.*" "pytest" "pyppeteer" \
+              "jupyter-server-proxy" "pyproj>=2.4.*"
 
 logger "Check versions..."
 python --version
