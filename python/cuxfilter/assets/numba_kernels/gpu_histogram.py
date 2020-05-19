2f8d2b782768826ce25a9cbab22cae0c69eb5074
Ajay Thorve <athorve@nvidia.com>
2020-05-18 17:44:11 -0700

    fixed dask_cudf groupby issue where it was computing aggregate function for all columns

--- python/cuxfilter/assets/numba_kernels/gpu_histogram.py
+++ python/cuxfilter/assets/numba_kernels/gpu_histogram.py
@@ -17,18 +17,22 @@ def calc_value_counts(a_gpu, stride, min_value, data_points):
         frequencies(ndarray), bin_edge_values(ndarray)
     """
     if type(a_gpu) == dask_cudf.core.Series:
-        if data_points == a_gpu.nunique().compute():
+        if stride is None:
             val_count = a_gpu.value_counts()
         else:
-            val_count = ((a_gpu / stride) - min_value).value_counts()
+            val_count = (
+                (a_gpu - min_value) / stride
+            ).astype('int').value_counts()
         val_count = val_count.compute().sort_index()
     else:
-        if data_points == a_gpu.nunique():
+        if stride is None:
             val_count = a_gpu.value_counts().sort_index()
         else:
-            val_count = ((a_gpu / stride) - min_value).value_counts()
+            val_count = (
+                (a_gpu - min_value) / stride
+            ).astype('int').value_counts().sort_index()
 
-    return val_count.index.to_array(), val_count.to_array()
+    return (val_count.index.to_array(), val_count.to_array()), len(val_count)
 
 
 def calc_groupby(chart: Type[BaseChart], data, agg=None):
@@ -41,17 +45,17 @@ def calc_groupby(chart: Type[BaseChart], data, agg=None):
     output:
         frequencies(ndarray), bin_edge_values(ndarray)
     """
-
     if agg is None:
-        temp_df = cudf.DataFrame()
-        temp_df.index = data.dropna(subset=[chart.x]).index
-        temp_df.add_column(
-            chart.x, (data[chart.x] / chart.stride) - chart.min_value,
-        )
-        temp_df.add_column(
-            chart.y, data.dropna(subset=[chart.x])[chart.y].copy()
-        )
-        if type(data) == dask_cudf.core.DataFrame:
+        temp_df = data[[chart.x, chart.y]].dropna(subset=[chart.x])
+        temp_df[chart.x] = (data[chart.x] / chart.stride) - chart.min_value
+
+        if type(temp_df) == dask_cudf.core.DataFrame:
+            groupby_res = getattr(
+                temp_df.groupby(by=[chart.x]),
+                chart.aggregate_fn
+            )()
+            groupby_res = groupby_res.reset_index().compute().to_pandas()
+        else:
             groupby_res = (
                 temp_df.groupby(by=[chart.x], as_index=False)
                 .agg({chart.y: chart.aggregate_fn})
@@ -60,9 +64,24 @@ def calc_groupby(chart: Type[BaseChart], data, agg=None):
         del temp_df
         gc.collect()
     else:
-        groupby_res = (
-            data.groupby(by=[chart.x], as_index=False).agg(agg).to_pandas()
-        )
+        if type(data) == dask_cudf.core.DataFrame:
+            groupby_res = None
+            for key, agg_fn in agg.items():
+                groupby_res_temp = getattr(
+                    data[[chart.x, key]].groupby(chart.x),
+                    agg_fn
+                )()
+                if groupby_res is None:
+                    groupby_res = groupby_res_temp.reset_index().compute()
+                else:
+                    groupby_res[key] = groupby_res_temp[key].compute()
+                del(groupby_res_temp)
+                gc.collect()
+            groupby_res = groupby_res.to_pandas()
+        else:
+            groupby_res = (
+                data.groupby(by=[chart.x], as_index=False).agg(agg).to_pandas()
+            )
     return groupby_res.to_numpy().transpose()
 
 
