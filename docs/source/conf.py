12caf13f141b53730e469ad509ad14b23d029ebf
gpuCI <38199262+GPUtester@users.noreply.github.com>
2020-06-03 10:38:27 -0700

    REL v0.14.0 release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,9 +27,9 @@ author = 'NVIDIA'
 # built documents.
 #
 # The short X.Y version.
-version = '0.15'
+version = '0.14'
 # The full version, including alpha/beta/rc tags
-release = '0.15.0'
+release = '0.14.0'
 
 nbsphinx_allow_errors = True
 # -- General configuration ---------------------------------------------------
