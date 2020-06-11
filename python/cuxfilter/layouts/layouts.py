0b93e2bf8d35098fbb450442fedbefb85ce41729
Bryan Van de Ven <bvandeven@nvidia.com>
2020-06-11 15:18:38 -0700

    some renames and further cleanup

--- python/cuxfilter/layouts/layouts.py
+++ python/cuxfilter/layouts/layouts.py
@@ -34,20 +34,21 @@ class _LayoutBase:
             "title", '<div class="nav-title"> ' + str(title) + "</div>"
         )
 
-        widgets = pn.Column()
+        widgets = [x for x in charts.values() if is_widget(x)]
+        plots = [x for x in charts.values() if not is_widget(x)]
 
         self._apply_themes(charts, theme)
-        self._process_widgets(charts, widgets)
+        widgetbox = self._process_widgets(widgets)
+        self._process_plots(plots, tmpl)
+        self._pad_missing_plots(len(plots), tmpl)
 
-        num_charts_added = self._process_charts(charts, tmpl)
-
-        self._pad_missing_charts(num_charts_added, tmpl)
-
-        tmpl.add_panel("widgets", widgets)
+        tmpl.add_panel("widgets", widgetbox)
         return tmpl
 
     @property
     def total_charts(self):
+        # Would be nice if the templates and this value could be derived from
+        # some common declarative source description
         return len(self._num_charts_pat.findall(self._layout))
 
     def _apply_themes(self, charts, theme):
@@ -55,54 +56,49 @@ class _LayoutBase:
             if hasattr(chart, "apply_theme"):
                 chart.apply_theme(theme.chart_properties)
 
-    def _process_widgets(self, charts, widgets):
-        for chart in (x for x in charts.values() if is_widget(x)):
-            chart.chart.width = 280
-            widgets.append(chart.view())
+    def _process_widgets(self, widgets):
+        widgets = pn.Column()
+        for obj in widgets:
+            obj.chart.width = 280
+            widgets.append(obj.view())
 
-    def _pad_missing_charts(self, num_charts_added, tmpl):
+    def _pad_missing_plots(self, num_charts_added, tmpl):
         N = self.total_charts - num_charts_added
 
         for i in range(N, num_charts_added, -1):
             tmpl.add_panel(f"chart{i}", "")
 
-    def _process_charts(self, charts, tmpl):
+    def _process_plots(self, plots, tmpl):
         raise NotImplementedError()
 
 
 class Layout0(_LayoutBase):
     _layout = layout_0
 
-    def _process_charts(self, charts, tmpl):
+    def _process_plots(self, plots, tmpl):
         """
         layout 0
         [1]
         """
 
-        charts = [x for x in charts.values() if not is_widget(x)]
-
-        for i, chart in enumerate(charts, 1):
+        for i, chart in enumerate(plots, 1):
             chart.width = 1600
             chart.height = 900
             chart.chart.sizing_mode = "scale_both"
             tmpl.add_panel(f"chart{i}", chart.view())
 
-        return len(charts)
-
 
 class Layout1(_LayoutBase):
     _layout = layout_1
 
-    def _process_charts(self, charts, tmpl):
+    def _process_plots(self, plots, tmpl):
         """
         layout 1
         [1]
         [2]
         """
 
-        charts = [x for x in charts.values() if not is_widget(x)]
-
-        for i, chart in enumerate(charts, 1):
+        for i, chart in enumerate(plots, 1):
             if i == 1:
                 chart.width = 1600
                 chart.height = 600
@@ -112,43 +108,35 @@ class Layout1(_LayoutBase):
             chart.chart.sizing_mode = "scale_both"
             tmpl.add_panel(f"chart{i}", chart.view())
 
-        return len(charts)
-
 
 class Layout2(_LayoutBase):
     _layout = layout_2
 
-    def _process_charts(self, charts, tmpl):
+    def _process_plots(self, plots, tmpl):
         """
         layout 2
 
         [1 2]
         """
 
-        charts = [x for x in charts.values() if not is_widget(x)]
-
-        for i, chart in enumerate(charts, 1):
+        for i, chart in enumerate(plots, 1):
             chart.width = 900
             chart.height = 900
             chart.chart.sizing_mode = "scale_both"
             tmpl.add_panel(f"chart{i}", chart.view())
 
-        return len(charts)
-
 
 class Layout3(_LayoutBase):
     _layout = layout_3
 
-    def _process_charts(self, charts, tmpl):
+    def _process_plots(self, plots, tmpl):
         """
         layout 3
         [1   2]
         [1   3]
         """
 
-        charts = [x for x in charts.values() if not is_widget(x)]
-
-        for i, chart in enumerate(charts, 1):
+        for i, chart in enumerate(plots, 1):
             if i == 1:
                 chart.width = 900
                 chart.height = 900
@@ -158,42 +146,34 @@ class Layout3(_LayoutBase):
             chart.chart.sizing_mode = "scale_both"
             tmpl.add_panel(f"chart{i}", chart.view())
 
-        return len(charts)
-
 
 class Layout4(_LayoutBase):
     _layout = layout_4
 
-    def _process_charts(self, charts, tmpl):
+    def _process_plots(self, plots, tmpl):
         """
         layout 4
         [1 2 3]
         """
 
-        charts = [x for x in charts.values() if not is_widget(x)]
-
-        for i, chart in enumerate(charts, 1):
+        for i, chart in enumerate(plots, 1):
             chart.width = int(1600 / 3)
             chart.height = int(1600 / 3)
             chart.chart.sizing_mode = "scale_both"
             tmpl.add_panel(f"chart{i}", chart.view())
 
-        return len(charts)
-
 
 class Layout5(_LayoutBase):
     _layout = layout_5
 
-    def _process_charts(self, charts, tmpl):
+    def _process_plots(self, plots, tmpl):
         """
         layout 5
         [  1  ]
         [2   3]
         """
 
-        charts = [x for x in charts.values() if not is_widget(x)]
-
-        for i, chart in enumerate(charts, 1):
+        for i, chart in enumerate(plots, 1):
             if i == 1:
                 chart.width = 1600
                 chart.height = 600
@@ -203,13 +183,11 @@ class Layout5(_LayoutBase):
             chart.chart.sizing_mode = "scale_both"
             tmpl.add_panel(f"chart{i}", chart.view())
 
-        return len(charts)
-
 
 class Layout6(_LayoutBase):
     _layout = layout_6
 
-    def _process_charts(self, charts, tmpl):
+    def _process_plots(self, plots, tmpl):
         """
         layout 6
 
@@ -217,21 +195,17 @@ class Layout6(_LayoutBase):
         [3  4]
         """
 
-        charts = [x for x in charts.values() if not is_widget(x)]
-
-        for i, chart in enumerate(charts, 1):
+        for i, chart in enumerate(plots, 1):
             chart.width = 800
             chart.height = 450
             chart.chart.sizing_mode = "scale_both"
             tmpl.add_panel(f"chart{i}", chart.view())
 
-        return len(charts)
-
 
 class Layout7(_LayoutBase):
     _layout = layout_7
 
-    def _process_charts(self, charts, tmpl):
+    def _process_plots(self, plots, tmpl):
         """
         layout 7
 
@@ -239,9 +213,7 @@ class Layout7(_LayoutBase):
         [2  3  4]
         """
 
-        charts = [x for x in charts.values() if not is_widget(x)]
-
-        for i, chart in enumerate(charts, 1):
+        for i, chart in enumerate(plots, 1):
             if i == 1:
                 chart.width = 1600
                 chart.height = 600
@@ -251,13 +223,11 @@ class Layout7(_LayoutBase):
             chart.chart.sizing_mode = "scale_both"
             tmpl.add_panel(f"chart{i}", chart.view())
 
-        return len(charts)
-
 
 class Layout8(_LayoutBase):
     _layout = layout_8
 
-    def _process_charts(self, charts, tmpl):
+    def _process_plots(self, plots, tmpl):
         """
         layout 8
 
@@ -265,9 +235,7 @@ class Layout8(_LayoutBase):
         [2  3   4  5]
         """
 
-        charts = [x for x in charts.values() if not is_widget(x)]
-
-        for i, chart in enumerate(charts, 1):
+        for i, chart in enumerate(plots, 1):
             if i == 1:
                 chart.width = 1600
                 chart.height = 600
@@ -277,13 +245,11 @@ class Layout8(_LayoutBase):
             chart.chart.sizing_mode = "scale_both"
             tmpl.add_panel(f"chart{i}", chart.view())
 
-        return len(charts)
-
 
 class Layout9(_LayoutBase):
     _layout = layout_9
 
-    def _process_charts(self, charts, tmpl):
+    def _process_plots(self, plots, tmpl):
         """
         layout 9
 
@@ -292,9 +258,7 @@ class Layout9(_LayoutBase):
         [4  5  6]
         """
 
-        charts = [x for x in charts.values() if not is_widget(x)]
-
-        for i, chart in enumerate(charts, 1):
+        for i, chart in enumerate(plots, 1):
             if i == 1:
                 chart.width = 1200
                 chart.height = 600
@@ -307,13 +271,11 @@ class Layout9(_LayoutBase):
             chart.chart.sizing_mode = "scale_both"
             tmpl.add_panel(f"chart{i}", chart.view())
 
-        return len(charts)
-
 
 class Layout10(_LayoutBase):
     _layout = layout_10
 
-    def _process_charts(self, charts, tmpl):
+    def _process_plots(self, plots, tmpl):
         """
         layout 10
 
@@ -321,21 +283,17 @@ class Layout10(_LayoutBase):
         [4  5  6]
         """
 
-        charts = [x for x in charts.values() if not is_widget(x)]
-
-        for i, chart in enumerate(charts, 1):
+        for i, chart in enumerate(plots, 1):
             chart.width = int(1600 / 3)
             chart.height = 450
             chart.chart.sizing_mode = "scale_both"
             tmpl.add_panel(f"chart{i}", chart.view())
 
-        return len(charts)
-
 
 class Layout11(_LayoutBase):
     _layout = layout_11
 
-    def _process_charts(self, charts, tmpl):
+    def _process_plots(self, plots, tmpl):
         """
         layout 11
 
@@ -343,9 +301,7 @@ class Layout11(_LayoutBase):
         [3   4  5   6]
         """
 
-        charts = [x for x in charts.values() if not is_widget(x)]
-
-        for i, chart in enumerate(charts, 1):
+        for i, chart in enumerate(plots, 1):
             if i in [1, 2]:
                 chart.width = int(1600 / 2)
                 chart.height = 600
@@ -355,13 +311,11 @@ class Layout11(_LayoutBase):
             chart.chart.sizing_mode = "scale_both"
             tmpl.add_panel(f"chart{i}", chart.view())
 
-        return len(charts)
-
 
 class Layout12(_LayoutBase):
     _layout = layout_12
 
-    def _process_charts(self, charts, tmpl):
+    def _process_plots(self, plots, tmpl):
         """
         layout 12
 
@@ -370,12 +324,8 @@ class Layout12(_LayoutBase):
         [7  8  9]
         """
 
-        charts = [x for x in charts.values() if not is_widget(x)]
-
-        for i, chart in enumerate(charts, 1):
+        for i, chart in enumerate(plots, 1):
             chart.width = int(1600 / 3)
             chart.height = 300
             chart.chart.sizing_mode = "scale_both"
             tmpl.add_panel(f"chart{i}", chart.view())
-
-        return len(charts)
