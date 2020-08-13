f7db4476fe5c0269e962a9534efd712bbfd14385
Ajay Thorve <athorve@nvidia.com>
2020-08-13 09:27:57 -0700

    fixing nested list warning for numpy 1.19

--- python/cuxfilter/charts/core/aggregate/core_aggregate_choropleth.py
+++ python/cuxfilter/charts/core/aggregate/core_aggregate_choropleth.py
@@ -148,7 +148,9 @@ class BaseChoropleth(BaseChart):
         self.geo_mapper = pd.DataFrame(
             {
                 self.x: np.array(list(self.geo_mapper.keys())),
-                "coordinates": np.array(list(self.geo_mapper.values())),
+                "coordinates": np.array(
+                    list(self.geo_mapper.values()), dtype=np.object
+                ),
             }
         )
 
