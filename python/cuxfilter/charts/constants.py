05a8c156d03ca9850e43234cffe954d3618e67b7
Ajay Thorve <athorve@nvidia.com>
2020-10-28 14:50:51 -0700

    replace list with tuple of constants

--- python/cuxfilter/charts/constants.py
+++ python/cuxfilter/charts/constants.py
@@ -6,8 +6,8 @@ CUXF_DEFAULT_COLOR_PALETTE = list(Viridis10)
 DATATILE_ACTIVE_COLOR = "#8ab4f7"
 DATATILE_INACTIVE_COLOR = "#d3d9e2"
 
-CUDF_DATETIME_TYPES = [
+CUDF_DATETIME_TYPES = tuple(
     "datetime64[{}]".format(i) for i in ["s", "ms", "us", "ns"]
-] + [np.datetime64]
+ ) + (np.datetime64, )
 
 CUDF_TIMEDELTA_TYPE = np.timedelta64
