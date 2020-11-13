f1643f35bb8e16db49836e0f5a3e2a4b0c24e227
Mark Sadang <marksadang@engineer.com>
2020-11-12 20:41:04 -0800

    Updated upload.sh and build.sh

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -5,17 +5,19 @@
 ######################################
 set -e
 
-# Logger function for build status output
-function gpuci_logger() {
-  echo -e "\n>>>> $@\n"
-}
-
 # Set path and build parallel level
 export PATH=/opt/conda/bin:/usr/local/cuda/bin:$PATH
-export PARALLEL_LEVEL=-4
+export PARALLEL_LEVEL=${PARALLEL_LEVEL:-4}
 
 # Set home to the job's workspace
 export HOME=$WORKSPACE
+
+# Determine CUDA release version
+export CUDA_REL=${CUDA_VERSION%.*}
+
+# Setup 'gpuci_conda_retry' for build retries (results in 2 total attempts)
+export GPUCI_CONDA_RETRY_MAX=1
+export GPUCI_CONDA_RETRY_SLEEP=30
 
 # Switch to project root; also root of repo checkout
 cd $WORKSPACE
@@ -25,25 +27,23 @@ if [[ "$BUILD_MODE" = "branch" && "$SOURCE_BRANCH" = branch-* ]] ; then
   export VERSION_SUFFIX=`date +%y%m%d`
 fi
 
-# Setup 'gpuci_conda_retry' for build retries (results in 2 total attempts)
-export GPUCI_CONDA_RETRY_MAX=1
-export GPUCI_CONDA_RETRY_SLEEP=30
-
 ################################################################################
 # SETUP - Check environment
 ################################################################################
 
-gpuci_logger "Get env"
+gpuci_logger "Check environment variables"
 env
 
 gpuci_logger "Activate conda env"
 . /opt/conda/etc/profile.d/conda.sh
 conda activate rapids
 
-gpuci_logger "Check versions"
+gpuci_logger "Check compiler versions"
 python --version
 $CC --version
 $CXX --version
+
+gpuci_logger "Check conda environment"
 conda info
 conda config --show-sources
 conda list --show-channel-urls
@@ -52,16 +52,16 @@ conda list --show-channel-urls
 conda config --set ssl_verify False
 
 ################################################################################
-# BUILD - Conda package build
+# BUILD - Conda package builds
 ################################################################################
 
-gpuci_logger "Build conda pkg for cuxfilter"
-source ci/cpu/cuxfilter/build_cuxfilter.sh
+echo "Building cuxfilter"
+gpuci_conda_retry build conda/recipes/cuxfilter --python=$PYTHON
 
 ################################################################################
 # UPLOAD - Conda packages
 ################################################################################
 
 gpuci_logger "Upload conda pkgs"
-source ci/cpu/upload_anaconda.sh
+source ci/cpu/upload.sh
 
--- ci/cpu/cuxfilter/build_cuxfilter.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-echo "Building cuxfilter"
-gpuci_conda_retry build conda/recipes/cuxfilter --python=$PYTHON
