5929373679560aa92d88264633da2b01816a0ccd
Ajay Thorve <athorve@nvidia.com>
2021-01-26 21:55:50 -0800

    black based formating

--- python/cuxfilter/dashboard.py
+++ python/cuxfilter/dashboard.py
@@ -12,7 +12,7 @@ from .charts.constants import (
     CUSTOM_DIST_PATH_THEMES,
     CUSTOM_DIST_PATH_LAYOUTS,
     STATIC_DIR_LAYOUT,
-    STATIC_DIR_THEMES
+    STATIC_DIR_THEMES,
 )
 from .datatile import DataTile
 from .layouts import single_feature
