82dc80c8824dad4d51f151a7addce82b831670c8
gpuCI <38199262+GPUtester@users.noreply.github.com>
2021-04-22 09:22:09 -0700

    REL v0.19.1 release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ author = 'NVIDIA'
 # The short X.Y version.
 version = '0.19'
 # The full version, including alpha/beta/rc tags
-release = '0.19.0'
+release = '0.19.1'
 
 nbsphinx_allow_errors = True
 # -- General configuration ---------------------------------------------------
