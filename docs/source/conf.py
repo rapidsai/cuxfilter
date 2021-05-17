a9d742f7fffa9afbe9a5b76815535d9196de5665
Raymond Douglass <ray@raydouglass.com>
2021-05-17 12:46:50 -0400

    DOC Update to v21.06.00

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,9 +27,9 @@ author = 'NVIDIA'
 # built documents.
 #
 # The short X.Y version.
-version = '0.20'
+version = '21.06'
 # The full version, including alpha/beta/rc tags
-release = '0.20.0'
+release = '21.06.00'
 
 nbsphinx_allow_errors = True
 # -- General configuration ---------------------------------------------------
