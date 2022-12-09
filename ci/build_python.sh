16f705d791de8e9ce11a75b147ce6e2e84b2843e
AJ Schmidt <aschmidt@nvidia.com>
2022-12-09 14:44:39 -0500

    remove some TODOs

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -11,10 +11,7 @@ rapids-print-env
 
 rapids-logger "Begin py build"
 
-# TODO: Remove `--no-test` flag once importing on a CPU
-# node works correctly
 rapids-mamba-retry mambabuild \
-  --no-test \
   conda/recipes/cuxfilter
 
 rapids-upload-conda-to-s3 python
