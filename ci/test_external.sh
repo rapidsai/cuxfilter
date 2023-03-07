c0ce6765ae3debf9830e26dfd23f685a9562734b
Ajay Thorve <ajaythorve1994@gmail.com>
2023-03-07 09:55:02 -0800

    add script to test hvplot, holoviews and datashader

---
+++ ci/test_external.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+# Copyright (c) 2023, NVIDIA CORPORATION.
+
+set -e
+
+rapids-logger "Create test_external conda environment"
+. /opt/conda/etc/profile.d/conda.sh
+
+rapids-mamba-retry create -n test_external -c rapidsai -c conda-forge -c nvidia  \
+    cudf=23.02 dask-cudf=23.02 python=3.10 cudatoolkit=11.8 \
+    dash pyct pytest pytest-benchmark pyviz_comms panel parameterized matplotlib \
+    colorcet xarray
+
+conda activate test_external
+
+# Define input parameter
+PROJECT=$1
+PR_NUMBER=$2
+LIBRARIES=("datashader" "holoviews" "hvplot")
+
+pushd /tmp
+
+# Clone the specified Python libraries
+if [ "$PROJECT" = "all" ]; then
+    # Loop through each library and install dependencies
+    for LIBRARY in "${LIBRARIES[@]}"
+    do
+        rapids-logger "Clone $LIBRARY"
+        # Clone the repository
+        git clone https://github.com/holoviz/$LIBRARY.git
+        # Change directory to the library
+        cd $LIBRARY
+
+        rapids-logger "Install $LIBRARY"
+        # Run setup.py
+        python setup.py install
+
+        cd ..
+    done
+else
+    rapids-logger "Clone $PROJECT"
+    git clone https://github.com/pyviz/$PROJECT.git
+
+    # Check if PR_NUMBER is a non-empty, valid number
+    if [ -n "$PR_NUMBER" ] && [ "$PR_NUMBER" -eq "$PR_NUMBER" ] 2>/dev/null; then
+        rapids-logger "checkout PR $PR_NUMBER"
+        # Fetch the pull request and check it out
+        git fetch origin pull/$PR_NUMBER/head:pr/$PR_NUMBER
+        git checkout pr/$PR_NUMBER
+    fi
+
+    # Change directory to the specified project
+    cd $PROJECT
+    rapids-logger "Install $PROJECT"
+    # Run setup.py
+    python setup.py install
+fi
+
+EXITCODE=0
+trap "EXITCODE=1" ERR
+set +e
+
+# Install and run tests
+if [ "$PROJECT" = "all" ]; then
+    # Loop through each library and install dependencies
+    for LIBRARY in "${LIBRARIES[@]}"
+    do
+        rapids-logger "run GPU tests for $LIBRARY"
+        # Change directory to the library
+        cd $LIBRARY
+
+        TEST_DIR="$LIBRARY/tests"
+
+        # Find all Python scripts containing the keywords cudf or dask_cudf
+        FILES=$(grep -l -R -e 'cudf' --include='*.py' "$TEST_DIR")
+
+        echo $FILES
+        DATASHADER_TEST_GPU=1 pytest $FILES
+        # Change directory back to the parent directory
+        cd ..
+    done
+else
+    rapids-logger "run GPU tests for $PROJECT"
+
+    TEST_DIR="$PROJECT/tests"
+
+    # Find all Python scripts containing the keywords cudf or dask_cudf
+    FILES=$(grep -l -R -e 'cudf' --include='*.py' "$TEST_DIR")
+
+    pytest $FILES
+fi
+
+rapids-logger "Test script exiting with value: $EXITCODE"
+exit ${EXITCODE}
