4c74d620e777f8f5220a57e2279a616745ee4adb
Divye Gala <divyegala@gmail.com>
2023-04-21 13:45:04 -0400

    replace/remove python 3.8 for 3.9

--- python/setup.py
+++ python/setup.py
@@ -17,7 +17,7 @@ setup(
         "Topic :: Scientific/Engineering",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
