62fde9d35a188a4d136a107743ed6ef3e5ea2582
Ray Douglass <ray@raydouglass.com>
2023-05-19 09:50:37 -0400

    DOC

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,9 +11,9 @@ author = "NVIDIA"
 # built documents.
 #
 # The short X.Y version.
-version = '23.06'
+version = '23.08'
 # The full version, including alpha/beta/rc tags
-release = '23.06.00'
+release = '23.08.00'
 
 nbsphinx_allow_errors = True
 # -- General configuration ---------------------------------------------------
