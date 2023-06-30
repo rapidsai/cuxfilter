595082fbe84d4efbfdb66672d532d3a80ffa4869
Ajay Thorve <ajaythorve1994@gmail.com>
2023-06-30 11:33:07 -0700

    add apply_wheel_modifications script

---
+++ ci/release/apply_wheel_modifications.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright (c) 2023, NVIDIA CORPORATION.
+#
+# Usage: bash apply_wheel_modifications.sh <new_version> <cuda_suffix>
+
+VERSION=${1}
+CUDA_SUFFIX=${2}
+
+# pyproject.toml versions
+sed -i "s/^version = .*/version = \"${VERSION}\"/g" python/cuxfilter/pyproject.toml
+
+# cuxfilter pyproject.toml cuda suffixes
+sed -i "s/^name = \"cuxfilter\"/name = \"cuxfilter${CUDA_SUFFIX}\"/g" python/cuxfilter/pyproject.toml
+# Need to provide the == to avoid modifying the URL
+sed -i "s/\"cudf==/\"cudf${CUDA_SUFFIX}==/g" python/cuxfilter/pyproject.toml
+sed -i "s/\"cuspatial==/\"cudf${CUDA_SUFFIX}==/g" python/cuxfilter/pyproject.toml
+sed -i "s/\"dask-cuda==/\"cudf${CUDA_SUFFIX}==/g" python/cuxfilter/pyproject.toml
+sed -i "s/\"dask-cudf==/\"cudf${CUDA_SUFFIX}==/g" python/cuxfilter/pyproject.toml
+
+if [[ $CUDA_SUFFIX == "-cu12" ]]; then
+    sed -i "s/numba[<=>\.,0-9]*/numba>=0.57/g" python/cuxfilter/pyproject.toml
+fi
