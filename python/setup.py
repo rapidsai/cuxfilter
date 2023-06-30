952d660134bc26bcfd3e227d6113647016114a65
Ajay Thorve <ajaythorve1994@gmail.com>
2023-06-30 10:50:12 -0700

    migrate to pyproject.toml

--- python/setup.py
+++ python/setup.py
@@ -1,36 +1,10 @@
 from setuptools import find_packages, setup
-import versioneer
-
-with open("README.md") as f:
-    readme = f.read()
 
 setup(
-    name="cuxfilter",
-    version=versioneer.get_version(),
-    description="GPU accelerated cross filtering with cuDF",
-    url="https://github.com/rapidsai/cuxfilter",
-    author="NVIDIA Corporation",
-    license="Apache 2.0",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Topic :: Database",
-        "Topic :: Scientific/Engineering",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
     packages=find_packages(
         include=["cuxfilter", "cuxfilter.*"],
         exclude=("tests", "docs", "notebooks"),
     ),
-    package_data=dict.fromkeys(
-        find_packages(
-            include=["cuxfilter.layouts.assets", "cuxfilter.themes.assets"]
-        ),
-        ["*.css", "*.html"],
-    ),
-    cmdclass=versioneer.get_cmdclass(),
+    package_data={"cuxfilter.themes.assets": ["*.css", "*.html"]},
     zip_safe=False,
 )
