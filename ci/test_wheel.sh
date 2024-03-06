b60886f1a7a2dbd27f9bcf334cf1f89d1b3964c8
Bradley Dice <bdice@bradleydice.com>
2024-03-06 11:23:57 -0800

    Treat cuxfilter CI artifacts as pure wheels (#577)
    This marks `cuxfilter` as a pure wheel, meaning that the CI artifacts are not specific to a Python version or CPU architecture. This change depends on https://github.com/rapidsai/gha-tools/pull/96, and makes CI workflows more robust by allowing the test matrix to be separated from the build matrix.
    
    Authors:
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - Jake Awe (https://github.com/AyodeAwe)
    
    URL: https://github.com/rapidsai/cuxfilter/pull/577

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -4,7 +4,7 @@
 set -eou pipefail
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-RAPIDS_PY_WHEEL_NAME="cuxfilter_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
+RAPIDS_PY_WHEEL_NAME="cuxfilter_${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_WHEEL_PURE="1" rapids-download-wheels-from-s3 ./dist
 
 # echo to expand wildcard before adding `[extra]` requires for pip
 python -m pip install $(echo ./dist/cuxfilter*.whl)[test]
