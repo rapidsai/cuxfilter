e7dc0f8778c8fce72d88ba3ad74b7e2e0a976e16
James Lamb <jlamb@nvidia.com>
2024-09-23 20:28:43 -0500

    remove scripts

--- ci/use_conda_packages_from_prs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-LIBCUSPATIAL_CHANNEL=$(rapids-get-pr-conda-artifact cuspatial 1441 cpp)
-CUSPATIAL_CHANNEL=$(rapids-get-pr-conda-artifact cuspatial 1441 python)
-
-conda config --system --add channels "${LIBCUSPATIAL_CHANNEL}"
-conda config --system --add channels "${CUSPATIAL_CHANNEL}"
