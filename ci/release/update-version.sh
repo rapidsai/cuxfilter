901a9b63d36548a515c0e6807eddf604548aa305
Ray Douglass <ray@raydouglass.com>
2024-10-09 16:43:44 -0400

    Fix update-version script for 24.10 release

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -48,7 +48,7 @@ DEPENDENCIES=(
   cuspatial
 )
 for DEP in "${DEPENDENCIES[@]}"; do
-  for FILE in dependencies.yaml conda/environments/*.yaml ci/utils/external_dependencies.yaml; do
+  for FILE in dependencies.yaml conda/environments/*.yaml; do
     sed_runner "/-.* ${DEP}\(-cu[[:digit:]]\{2\}\)\{0,1\}==/ s/==.*/==${NEXT_SHORT_TAG_PEP440}.*,>=0.0.0a0/g" "${FILE}"
   done
   for FILE in python/pyproject.toml; do
