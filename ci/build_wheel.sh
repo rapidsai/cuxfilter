92dd262ddaa61bca5cc36493f354afa85189d0d7
James Lamb <jlamb@nvidia.com>
2024-11-14 12:18:16 -0600

    enforce wheel size limits, README formatting in CI (#643)
    Contributes to https://github.com/rapidsai/build-planning/issues/110
    
    Proposes adding 2 types of validation on wheels in CI, to ensure we continue to produce wheels that are suitable for PyPI.
    
    * checks on wheel size (compressed),
      - *to be sure they're under PyPI limits*
      - *and to prompt discussion on PRs that significantly increase wheel sizes*
    * checks on README formatting
      - *to ensure they'll render properly as the PyPI project homepages*
      - *e.g. like how https://github.com/scikit-learn/scikit-learn/blob/main/README.rst becomes https://pypi.org/project/scikit-learn/*
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
      - Ajay Thorve (https://github.com/AjayThorve)
    
    URL: https://github.com/rapidsai/cuxfilter/pull/643

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -16,4 +16,6 @@ cd "${package_dir}"
 
 python -m pip wheel . -w dist -v --no-deps --disable-pip-version-check
 
+../ci/validate_wheel.sh dist
+
 RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_WHEEL_PURE="1" rapids-upload-wheels-to-s3 dist
