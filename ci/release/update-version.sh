4a7b354604fcd340c0cbb04071105bb7f8b6b9e2
Gil Forsyth <gforsyth@nvidia.com>
2025-07-17 13:08:08 -0400

    "fix(docker): use versioned `-latest` tag for all `rapidsai` images"

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -59,4 +59,8 @@ sed_runner "/cuxfilter=[0-9]\{2\}.[0-9]\{2\}/ s/=[0-9]\{2\}.[0-9]\{2\}/=${NEXT_S
 # CI files
 for FILE in .github/workflows/*.yaml; do
   sed_runner "/shared-workflows/ s/@.*/@branch-${NEXT_SHORT_TAG}/g" "${FILE}"
+  sed_runner "s/:[0-9]*\\.[0-9]*-/:${NEXT_SHORT_TAG}-/g" "${FILE}"
 done
+
+# Update CI image tags of the form {rapids_version}-{something}
+sed_runner "s/:[0-9]*\\.[0-9]*-/:${NEXT_SHORT_TAG}-/g" ./CONTRIBUTING.md
