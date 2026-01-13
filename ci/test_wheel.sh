0eb4b624c092a75959796ad093acf280e337cd4e
Gil Forsyth <gforsyth@nvidia.com>
2026-01-13 09:54:20 -0500

    test(sabi): test cuxfilter against upstream SABI wheels

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
 set -eou pipefail
@@ -7,6 +7,10 @@ set -eou pipefail
 source rapids-init-pip
 
 CUXFILTER_WHEELHOUSE=$(rapids-download-from-github "$(rapids-package-name wheel_python cuxfilter --pure --cuda "${RAPIDS_CUDA_VERSION}")")
+
+if [[ "${RAPIDS_PY_VERSION}" != "3.10" ]]; then
+    source ./ci/use_upstream_sabi_wheels.sh
+fi
 
 # echo to expand wildcard before adding `[extra]` requires for pip
 rapids-pip-retry install "$(echo "${CUXFILTER_WHEELHOUSE}"/cuxfilter*.whl)[test]"
