c2c896d832dc104ee7bcab9c060db2c24c442444
James Lamb <jaylamb20@gmail.com>
2026-03-18 20:54:54 -0500

    build wheels with CUDA 13.0.x, test wheels against mix of CTK versions (#777)
    Contributes to https://github.com/rapidsai/build-planning/issues/257
    
    * builds CUDA 13 wheels with the 13.0 CTK
    
    Contributes to https://github.com/rapidsai/build-planning/issues/256
    
    * updates wheel tests to cover a range of CTK versions (we previously, accidentally, were only testing the latest 12.x and 13.x)
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Mike Sarahan (https://github.com/msarahan)
    
    URL: https://github.com/rapidsai/cuxfilter/pull/777

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -8,7 +8,18 @@ source rapids-init-pip
 
 CUXFILTER_WHEELHOUSE=$(rapids-download-from-github "$(rapids-package-name wheel_python cuxfilter --pure --cuda "${RAPIDS_CUDA_VERSION}")")
 
-# echo to expand wildcard before adding `[extra]` requires for pip
-rapids-pip-retry install "$(echo "${CUXFILTER_WHEELHOUSE}"/cuxfilter*.whl)[test]"
+# generate constraints (possibly pinning to oldest support versions of dependencies)
+rapids-generate-pip-constraints test_python "${PIP_CONSTRAINT}"
+
+# notes:
+#
+#   * echo to expand wildcard before adding `[test]` requires for pip
+#   * just providing --constraint="${PIP_CONSTRAINT}" to be explicit, and because
+#     that environment variable is ignored if any other --constraint are passed via the CLI
+#
+rapids-pip-retry install \
+    --prefer-binary \
+    --constraint "${PIP_CONSTRAINT}" \
+    "$(echo "${CUXFILTER_WHEELHOUSE}"/cuxfilter*.whl)[test]"
 
 python -m pytest -n 8 ./python/cuxfilter/tests
